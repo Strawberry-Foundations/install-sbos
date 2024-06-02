b2602d996be019007ca893198ba1d89115e4a4c0
Juliandev02 <julian@strawberryfoundations.xyz>
2024-06-02 18:58:04 +0200

    added custom partitioning

--- sbos_installer/steps/overview.py
+++ sbos_installer/steps/overview.py
@@ -6,6 +6,8 @@ from sbos_installer.utils.colors import *
 def overview(data, disk):
     print(f"\n{GREEN}{BOLD} -- Install overview --{CRESET}")
 
+    disk_data = data['disk'][disk]
+
     print(f"{GRAY}{BOLD}* {CYAN}Hostname:{CRESET} {data['hostname']}")
     print(f"{GRAY}{BOLD}* {CYAN}Network interface:{CRESET} {data['net_interface']}")
     print(f"{GRAY}{BOLD}* {CYAN}Timezone:{CRESET} {data['timezone']['region']}/{data['timezone']['city']}")
@@ -13,10 +15,10 @@ def overview(data, disk):
     for user in data["users"]:
         print(f"{GRAY}{BOLD}    * {CYAN}{user}{CRESET}")
     print(f"{GRAY}{BOLD}* {CYAN}Disk:{CRESET} {disk}")
-    print(f"{GRAY}{BOLD}    * {CYAN}EFI:{CRESET} {data['disk'][disk]['efi_disk_size']}G{CRESET}")
-    print(f"{GRAY}{BOLD}    * {CYAN}System:{CRESET} {data['disk'][disk]['system_disk_size']}G{CRESET}")
-    print(f"{GRAY}{BOLD}    * {CYAN}User:{CRESET} {data['disk'][disk]['user_disk_size']}G{CRESET}")
-    print(f"{GRAY}{BOLD}    * {CYAN}Swap:{CRESET} {data['disk'][disk]['swap_disk_size']}G{CRESET}")
+    print(f"{GRAY}{BOLD}    * {CYAN}EFI on {CYAN}{disk_data['efi']['block']}{CRESET}: {disk_data['efi']['size']}G{CRESET}")
+    print(f"{GRAY}{BOLD}    * {CYAN}System on {CYAN}{disk_data['system']['block']}{CRESET}: {disk_data['system']['size']}G{CRESET}")
+    print(f"{GRAY}{BOLD}    * {CYAN}User on {CYAN}{disk_data['user']['block']}{CRESET}: {disk_data['user']['size']}G{CRESET}")
+    print(f"{GRAY}{BOLD}    * {CYAN}Swap on {CYAN}{disk_data['swap']['block']}{CRESET}: {disk_data['swap']['size']}G{CRESET}")
     print(f"{GRAY}{BOLD}* {CYAN}Packages:{CRESET} {', '.join(data['packages'])}")
 
     confirm = parse_bool(ia_selection("\nDo you want to continue?", options=["Yes", "No"]))
