86950568c289d009705fa3cc3faacfca24595a15
Juliandev02 <julian@strawberryfoundations.xyz>
2024-06-02 22:17:46 +0200

    added core.process

---
+++ sbos_installer/core/process.py
@@ -0,0 +1,9 @@
+import subprocess
+
+
+def run(command):
+    try:
+        result = subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        return result.stdout
+    except subprocess.CalledProcessError as e:
+        return f"Command '{command}' failed with error:\n{e.stderr}"
