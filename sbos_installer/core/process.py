17a286ca6017ac731da7ebab821fee742e4879c4
Juliandev02 <julian@strawberryfoundations.xyz>
2024-06-02 22:58:43 +0200

    bug fix

--- sbos_installer/core/process.py
+++ sbos_installer/core/process.py
@@ -21,17 +21,17 @@ class Runner:
     def __init__(self, show_logs: bool):
         self.show_logs = show_logs
 
-    def run(self, command: str):
+    def run(self, run_command: str):
         try:
             if self.show_logs:
-                _command = subprocess.run(command, shell=True)
+                command = subprocess.run(run_command, shell=True)
 
-                if _command.returncode != 0:
+                if command.returncode != 0:
                     print(f"{YELLOW}{BOLD}Something went wrong while installing StrawberryOS ...{CRESET}")
                     sys.exit(1)
             else:
-                result = subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                result = subprocess.run(run_command, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
                                         text=True)
                 return result.stdout
         except subprocess.CalledProcessError as e:
-            return f"Command '{command}' failed with error:\n{e.stderr}"
+            return f"Command '{run_command}' failed with error:\n{e.stderr}"
