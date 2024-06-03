f30675a8366b20877096ec25d48c461cd41fdf08
Juliandev02 <julian@strawberryfoundations.xyz>
2024-06-03 17:50:52 +0200

    Bug fix (test)

--- sbos_installer/core/packages.py
+++ sbos_installer/core/packages.py
@@ -1,7 +1,7 @@
 init_package = (
     "dbus,dbus-bin,dbus-daemon,dbus-session-bus-common,dbus-system-bus-common,dbus-user-session,"
-    "libpam-systemd, apt-utils,apt-transport-https,ca-certificates,bash,bzip2,initramfs-tools-core,"
-    "initramfs-tools,linux-image-amd64, busybox-static,network-manager,wget,curl"
+    "libpam-systemd,apt-utils,apt-transport-https,ca-certificates,bash,bzip2,initramfs-tools-core,"
+    "initramfs-tools,linux-image-amd64,busybox-static,network-manager,wget,curl"
 )
 
 package_list = {
