5c391b765562d8f552ba19cfdf91e13717ede541
Juliandev02 <julian@strawberryfoundations.xyz>
2024-06-03 18:16:31 +0200

    Added development flags

--- sbos_installer/steps/disk.py
+++ sbos_installer/steps/disk.py
@@ -1,6 +1,7 @@
 from sbos_installer.cli.selection import ia_selection
 from sbos_installer.cli.parser import parse_bool
 from sbos_installer.utils.colors import *
+from sbos_installer.dev import DEV_FLAG_SKIP_DISK_INPUT
 
 import subprocess
 import os
@@ -49,6 +50,35 @@ def disk_partitioning():
     block, block_size, all_blocks = get_block_devices()
 
     disk = ia_selection("Select the disk you want to use for Strawberry OS", options=block, flags=block_size)
+
+    if DEV_FLAG_SKIP_DISK_INPUT:
+        efi_disk = "/dev/sda1"
+        system_disk = "/dev/sda2"
+        user_disk = "/dev/sda4"
+        swap_disk = "/dev/sda3"
+
+        return {
+            "disk": {
+                disk: {
+                    "efi": {
+                        "block": efi_disk,
+                        "size": get_block_device_size_in_gb(efi_disk)
+                    },
+                    "system": {
+                        "block": system_disk,
+                        "size": get_block_device_size_in_gb(system_disk)
+                    },
+                    "user": {
+                        "block": user_disk,
+                        "size": get_block_device_size_in_gb(user_disk)
+                    },
+                    "swap": {
+                        "block": swap_disk,
+                        "size": get_block_device_size_in_gb(swap_disk)
+                    }
+                }
+            }
+        }, disk
 
     own_partitioning = ia_selection(
         question=f"\nWould you like to create your own partitioning for {disk}?",
@@ -108,15 +138,10 @@ def disk_partitioning():
         if existing_partition:
             print("Please enter the following partitions with the correct block device to continue the partitioning.\n")
 
-            # efi_disk = input(f"EFI disk (e.g. {CYAN}/dev/sda1{CRESET}): ")
-            # system_disk = input(f"System disk (e.g. {CYAN}/dev/sda2{CRESET}): ")
-            # user_disk = input(f"User disk (e.g. {CYAN}/dev/sda3{CRESET}): ")
-            # swap_disk = input(f"Swap disk (e.g. {CYAN}/dev/sda4{CRESET}): ")
-
-            efi_disk = "/dev/sda1"
-            system_disk = "/dev/sda2"
-            user_disk = "/dev/sda4"
-            swap_disk = "/dev/sda3"
+            efi_disk = input(f"EFI disk (e.g. {CYAN}/dev/sda1{CRESET}): ")
+            system_disk = input(f"System disk (e.g. {CYAN}/dev/sda2{CRESET}): ")
+            user_disk = input(f"User disk (e.g. {CYAN}/dev/sda3{CRESET}): ")
+            swap_disk = input(f"Swap disk (e.g. {CYAN}/dev/sda4{CRESET}): ")
 
             return {
                 "disk": {
