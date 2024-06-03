e498ccf939c859cf567132d8092b43403f5ad8a1
Juliandev02 <julian@strawberryfoundations.xyz>
2024-06-03 19:25:12 +0200

    Fixed a bug

--- sbos_installer/steps/bootloader.py
+++ sbos_installer/steps/bootloader.py
@@ -5,7 +5,7 @@ from sbos_installer.utils.colors import *
 def configure_bootloader(disk: str):
     runner = Runner(True)
     print(f"{BOLD}{GREEN}Configuring GRUB ...{CRESET}")
-    with open("/mnt/etc/default/grub", "+") as _grub_default:
+    with open("/mnt/etc/default/grub", "w+") as _grub_default:
         _content = _grub_default.read()
         _content = _content.replace('GRUB_CMDLINE_LINUX=""', 'GRUB_CMDLINE_LINUX="overlay=yes"')
         _grub_default.write(_content)
