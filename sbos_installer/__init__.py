5d7915957f1fea546dd9d67ce6afde79e169fa69
Juliandev02 <julian@strawberryfoundations.xyz>
2024-06-12 22:54:50 +0200

    impl: Updated version

--- sbos_installer/__init__.py
+++ sbos_installer/__init__.py
@@ -17,7 +17,7 @@ from sbos_installer.steps.general import configure_desktop
 
 import sys
 
-version = "1.0.3"
+version = "1.0.4"
 
 if not check_root_permissions():
     print(f"{BOLD}{RED}Requires root permissions{CRESET}")
