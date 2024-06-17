b1fdbf7d80fc87c9541f32f517375ba3a8c1fc7a
Juliandev02 <julian@strawberryfoundations.xyz>
2024-06-17 22:58:57 +0200

    impl: Moved dev flag argument parsing to dev.py

--- sbos_installer/dev.py
+++ sbos_installer/dev.py
@@ -1,6 +1,30 @@
-DEV_FLAG_DEV_MODE = False
+import sys
 
+args = sys.argv[1:]
+
+DEV_FLAG_DEV_MODE = False
 DEV_FLAG_SKIP_DISK_INPUT = False
 DEV_FLAG_SKIP_BOOTSTRAP = False
 DEV_FLAG_SKIP_INITRAMFS = False
 DEV_FLAG_SKIP_POST_SETUP = False
+
+if '--enable-dev' in args:
+    ENABLE_DEV_MODE = True
+
+if any(arg.startswith('-f') and 'DEV_FLAG_SKIP_BOOTSTRAP' in arg for arg in args):
+    DEV_FLAG_SKIP_BOOTSTRAP = True
+
+if any(arg.startswith('-f') and 'DEV_FLAG_SKIP_INITRAMFS' in arg for arg in args):
+    DEV_FLAG_SKIP_INITRAMFS = True
+
+if any(arg.startswith('-f') and 'DEV_FLAG_SKIP_DISK_INPUT' in arg for arg in args):
+    DEV_FLAG_SKIP_DISK_INPUT = True
+
+if any(arg.startswith('-f') and 'DEV_FLAG_SKIP_POST_SETUP' in arg for arg in args):
+    DEV_FLAG_SKIP_POST_SETUP = True
+
+if DEV_FLAG_DEV_MODE:
+    DEV_FLAG_SKIP_BOOTSTRAP = True
+    DEV_FLAG_SKIP_INITRAMFS = True
+    DEV_FLAG_SKIP_DISK_INPUT = True
+    DEV_FLAG_SKIP_POST_SETUP = True
