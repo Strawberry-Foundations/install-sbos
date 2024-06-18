5d7c2be93c7733138d043e89a4421edee790c04d
Juliandev02 <julian@strawberryfoundations.xyz>
2024-06-18 11:16:54 +0200

    impl: moved _modify_grub_config to utils + renamed it to modify_file_entry()

--- sbos_installer/steps/bootloader.py
+++ sbos_installer/steps/bootloader.py
@@ -1,17 +1,8 @@
 from sbos_installer.cli.parser import parse_bool
 from sbos_installer.cli.selection import ia_selection
 from sbos_installer.core.process import Runner
+from sbos_installer.utils import modify_file_entry
 from sbos_installer.utils.colors import *
-
-
-def _modify_grub_config(file_path, search_string, replace_string):
-    with open(file_path, 'r') as file:
-        content = file.read()
-
-    modified_content = content.replace(search_string, replace_string)
-
-    with open(file_path, 'w') as file:
-        file.write(modified_content)
 
 
 def configure_bootloader(disk: str):
@@ -30,15 +21,15 @@ def configure_bootloader(disk: str):
 def _configure_grub(disk: str):
     runner = Runner(True)
 
-    _modify_grub_config("/mnt/etc/default/grub", 'GRUB_CMDLINE_LINUX=""', 'GRUB_CMDLINE_LINUX="overlay=yes"')
-    _modify_grub_config("/mnt/etc/default/grub", 'Debian', 'StrawberryOS')
+    modify_file_entry("/mnt/etc/default/grub", 'GRUB_CMDLINE_LINUX=""', 'GRUB_CMDLINE_LINUX="overlay=yes"')
+    modify_file_entry("/mnt/etc/default/grub", 'Debian', 'StrawberryOS')
 
     print(f"{BOLD}{GREEN}Installing GRUB ...{CRESET}")
     runner.run(
         f"grub-install --efi-directory=/mnt/boot/efi --boot-directory=/mnt/boot/ --bootloader-id=StrawberryOS {disk}")
     runner.run(f"chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg")
 
-    _modify_grub_config("/mnt/boot/grub/grub.cfg", 'GNU/Linux', '(Chocolate Crisps)')
+    modify_file_entry("/mnt/boot/grub/grub.cfg", 'GNU/Linux', '(Chocolate Crisps)')
 
 
 def _configure_systemd_boot():
