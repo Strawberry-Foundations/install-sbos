30b61ed9d7527110d1cec621f5aa27ae948fecdc
Juliandev02 <julian@strawberryfoundations.xyz>
2024-07-02 20:04:25 +0200

    impl: lvm & partitioning configurator will now use the sizes from disk setup

--- sbos_installer/__init__.py
+++ sbos_installer/__init__.py
@@ -5,7 +5,7 @@ from sbos_installer.cli.selection import ia_selection
 from sbos_installer.utils.colors import *
 from sbos_installer.dev import *
 
-from sbos_installer.steps.disk import disk_partitioning
+from sbos_installer.steps.disk import disk_partitioning, configure_partitions
 from sbos_installer.steps.lvm import configure_lvm
 from sbos_installer.steps.hostname import setup_hostname
 from sbos_installer.steps.network import setup_network
@@ -17,6 +17,7 @@ from sbos_installer.steps.bootloader import configure_bootloader
 from sbos_installer.steps.general import configure_desktop
 
 import sys
+import time
 
 version = "1.1.5"
 
@@ -75,7 +76,9 @@ try:
     overview(install_data, disk)
 
     if not disk_data["disk"]["custom_partitioning"]:
-        configure_lvm(disk)
+        configure_partitions(disk, install_data)
+        time.sleep(0.5)
+        configure_lvm(disk, install_data)
 
         run(f"mount --mkdir /dev/strawberryos/system /mnt")
         run(f"mount --mkdir {install_data['disk'][disk]['efi']['block']} /mnt/boot/efi")
