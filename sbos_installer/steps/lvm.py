730f41f7b97529926a0c6b2666c6fc9e342623bc
Juliandev02 <julian@strawberryfoundations.xyz>
2024-07-02 20:04:23 +0200

    impl: lvm & partitioning configurator will now use the sizes from disk setup

--- sbos_installer/steps/lvm.py
+++ sbos_installer/steps/lvm.py
@@ -1,7 +1,5 @@
-from sbos_installer.core.process import Runner, run
+from sbos_installer.core.process import Runner
 from sbos_installer.utils.colors import *
-
-import time
 
 
 def get_partition_suffix(device):
@@ -11,27 +9,19 @@ def get_partition_suffix(device):
         return ""
 
 
-def configure_lvm(device):
+def configure_lvm(disk, disk_data):
     runner = Runner(True)
-    suffix = get_partition_suffix(device)
-
-    print(f"\n{GREEN}{BOLD}Creating partitions ...{CRESET}")
-    runner.run(f"parted -s {device} mklabel gpt")
-    runner.run(f"parted -s -a optimal {device} mkpart primary fat32 1MiB 513MiB")
-    runner.run(f"parted -s -a optimal {device} mkpart primary linux-swap 513MiB 2561MiB")
-    runner.run(f"parted -s -a optimal {device} mkpart primary 2561MiB 100%")
-
-    runner.run(f"sync")
-    time.sleep(0.5)
+    suffix = get_partition_suffix(disk)
+    disk_data = disk_data['disk'][disk]
 
     print(f"\n{GREEN}{BOLD}Formatting base partitions ...{CRESET}")
-    runner.run(f"mkfs.fat -F 32 {device}{suffix}1")
-    runner.run(f"mkswap {device}{suffix}2")
+    runner.run(f"mkfs.fat -F 32 {disk}{suffix}1")
+    runner.run(f"mkswap {disk}{suffix}2")
 
     print(f"\n{GREEN}{BOLD}Creating LVM group ...{CRESET}")
-    runner.run(f"pvcreate {device}{suffix}3 -ff -y ")
-    runner.run(f"vgcreate strawberryos {device}{suffix}3")
-    runner.run(f"lvcreate -L 10G -n system strawberryos")
+    runner.run(f"pvcreate {disk}{suffix}3 -ff -y ")
+    runner.run(f"vgcreate strawberryos {disk}{suffix}3")
+    runner.run(f"lvcreate -L {disk_data['system']['size']}M -n system strawberryos")
     runner.run(f"lvcreate -l 100%FREE -n user strawberryos")
 
     print(f"\n{GREEN}{BOLD}Formatting LVM volumes ...{CRESET}")
