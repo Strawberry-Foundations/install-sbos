1c2c7599ec90e41abf2329b594da128e00593b78
Juliandev02 <julian@strawberryfoundations.xyz>
2024-07-02 17:03:14 +0200

    fix: Fixed /etc/issue

--- sbos_installer/__init__.py
+++ sbos_installer/__init__.py
@@ -119,6 +119,7 @@ try:
 
     with open("/mnt/etc/issue", 'w') as file:
         file.write(r"StrawberryOS Chocolate Crisps \n \l"
+                   "\n"
                    r"")
 
     # todo: Add StrawberryOS recovery (custom initramfs?)
