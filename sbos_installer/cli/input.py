b2a2541bfa6fdd5958d60fc211f45567e65724c7
Juliandev02 <julian@strawberryfoundations.xyz>
2024-07-02 18:36:44 +0200

    impl: Updated ValueError raiser

--- sbos_installer/cli/input.py
+++ sbos_installer/cli/input.py
@@ -2,11 +2,11 @@ def parse_size(input_str):
     input_str = input_str.strip().lower()
 
     if input_str.endswith('g'):
-        size_in_gb = float(input_str[:-2])
+        size_in_gb = float(input_str[:-1])
         size_in_mb = int(size_in_gb * 1024)
     elif input_str.endswith('m'):
-        size_in_mb = int(float(input_str[:-2]))
+        size_in_mb = int(float(input_str[:-1]))
     else:
-        raise ValueError("Invalid format")
+        return None
 
     return size_in_mb
