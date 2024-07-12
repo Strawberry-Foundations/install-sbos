c487a4152824a3a1e56ac7e14adb5984bd4e5eeb
Juliandev02 <julian@strawberryfoundations.xyz>
2024-07-12 18:05:18 +0200

    feat: added views

--- sbos_installer/core/ui/screen.py
+++ sbos_installer/core/ui/screen.py
@@ -1,6 +1,21 @@
+from sbos_installer.core.ui.header import Header
+from sbos_installer.utils.screen import clear_screen
+
+from rich.console import Console
+
+
 class Screen:
-    def __init__(self, title):
+    title = ""
+    console = Console()
+
+    def __init__(self, title, view):
         self.title = title
+        self.view = view
+
+        clear_screen()
+        Header(self.title)
+
+        self.render()
 
     def render(self):
-        pass
+        self.view()
