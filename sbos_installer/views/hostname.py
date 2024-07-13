913121ec9d709c1d8ae3d8527102f86a3968cc93
Juliandev02 <julian@strawberryfoundations.xyz>
2024-07-13 22:22:42 +0200

    feat: finalized hostname screen

--- sbos_installer/views/hostname.py
+++ sbos_installer/views/hostname.py
@@ -1,16 +1,36 @@
 from sbos_installer.core.ui.screen import Screen
+from sbos_installer.utils.colors import *
+
+from rich.text import Text
+from rich.padding import Padding
 
 
 class HostnameView(Screen):
     title = "Configure hostname"
+    hostname = ""
 
     def __init__(self):
         view = self.render
         super().__init__(title=self.title, view=view)
 
+    def input(self):
+        self.console.print(Padding(
+            Text.from_ansi(f"{YELLOW}[!] {GRAY}'{GREEN}strawberryos{GRAY}' is automatically used if no input is made\n{CRESET}"),
+            (0, 18))
+        )
+
+        self.hostname = input(f"        Hostname:  ")
+
     def render(self):
-        hostname = input("Enter system hostname [strawberryos]: ")
-        if hostname.strip() == "":
+        self.console.print(Padding(
+            "Enter the hostname for your new system.\n"
+            "The host name can consist of numbers, upper and lower case letters.\n",
+            (0, 8))
+        )
+
+        self.input()
+
+        if self.hostname.strip() == "":
             return "strawberryos"
 
-        return hostname
+        return self.hostname
