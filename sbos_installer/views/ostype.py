7fae9944c98e08188c267c153d9ff758bdff5a59
Juliandev02 <julian@strawberryfoundations.xyz>
2024-07-13 21:29:25 +0200

    feat: added versions

--- sbos_installer/views/ostype.py
+++ sbos_installer/views/ostype.py
@@ -1,8 +1,6 @@
 from sbos_installer.core.ui.select_button import SelectButton, ia_selection
 from sbos_installer.core.ui.screen import Screen
 
-from rich import print as rprint
-
 
 class OSTypeView(Screen):
     title = "Choose the type of installation"
@@ -18,21 +16,21 @@ class OSTypeView(Screen):
         group = []
 
         SelectButton(
-            label="StrawberryOS Desktop",
+            label=f"StrawberryOS Desktop (2024.07)",
             description="The standard version of StrawberryOS - With all the necessary tools "
                         "from us and a selection of desktops",
             group=group
         )
 
         SelectButton(
-            label="StrawberryOS Desktop with Open Directory",
+            label=f"StrawberryOS Desktop with Open Directory (2024.07)",
             description="StrawberryOS with configured Open Directory. Useful for schools, workplaces, and "
             "also the one or other private use",
             group=group
         )
 
         SelectButton(
-            label="StrawberryOS Server",
+            label=f"StrawberryOS Server (2024.1)",
             description="A minimal environment of StrawberryOS without a desktop. Includes additional server utilities",
             group=group
         )
