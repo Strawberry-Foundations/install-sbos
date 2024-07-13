23ed811fde851996a9e9870bfc051c4baef09b2e
Juliandev02 <julian@strawberryfoundations.xyz>
2024-07-13 23:59:00 +0200

    feat: added users

---
+++ sbos_installer/views/user.py
@@ -0,0 +1,97 @@
+from sbos_installer import ia_selection
+from sbos_installer.cli.parser import parse_bool
+from sbos_installer.core.ui.screen import Screen
+from sbos_installer.utils.colors import *
+
+from rich.text import Text
+from rich.padding import Padding
+from getpass import getpass
+
+
+class UserView(Screen):
+    title = "Setting up users"
+
+    def __init__(self):
+        view = self.render
+        super().__init__(title=self.title, view=view)
+
+    def render(self):
+        self.console.print(Padding(
+            "User accounts are an important part of installing an operating system. "
+            "First enter a new root password, which is used for the root user.\n",
+            (0, 8))
+        )
+
+        root_password = None
+
+        username = None
+        password = None
+
+        while True:
+            root_password = self.setup_password()
+            if root_password.strip() == "":
+                self.console.print(Padding(Text.from_ansi(f"{YELLOW}{BOLD}Password cannot be empty{CRESET}"), (0, 8)))
+                continue
+            break
+
+        new_user = parse_bool(ia_selection("Do you want to create a new user?", options=["Yes", "No"], padding=8))
+
+        if new_user:
+            _username = True
+            while _username:
+                username = input(f"        {CRESET}Enter username:  {GRAY}")
+                if username.strip() == "":
+                    self.console.print(Padding(Text.from_ansi(f"{YELLOW}{BOLD}Username cannot be empty{CRESET}"), (0, 8)))
+                else:
+                    _username = False
+
+            pw = True
+            while pw:
+                password = self.setup_password(name=username)
+                if password.strip() == "":
+                    self.console.print(Padding(Text.from_ansi(f"{YELLOW}{BOLD}Password cannot be empty{CRESET}"), (0, 8)))
+                else:
+                    pw = False
+
+            print()
+            add_to_sudo = parse_bool(ia_selection(
+                question=f"Would you like to add '{CYAN}{BOLD}{username}{CRESET}' to the »{GREEN} sudo {CRESET}« group?",
+                options=["Yes", "No"],
+                padding=8
+            ))
+
+            user_setup = {
+                "users": {
+                    "root": {
+                        "password": root_password
+                    },
+                    username: {
+                        "password": password,
+                        "sudo_user": add_to_sudo
+                    }
+                }
+            }
+        else:
+            user_setup = {
+                "users": {
+                    "root": {
+                        "password": root_password
+                    },
+                }
+            }
+
+        return user_setup
+
+        print(CRESET)
+
+        return ""
+
+    def setup_password(self, name="root"):
+        _password = getpass(f"        Password for {name}: ")
+        _password_confirm = getpass(f"        Confirm password for {name}: ")
+
+        if _password != _password_confirm:
+            self.console.print(Padding(Text.from_ansi(f"{YELLOW}{BOLD}Passwords does not match{CRESET}"), (0, 8)))
+            self.setup_password()
+
+        return _password
