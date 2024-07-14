0b8018650ef7074b341dd5c8a8b7a1165448e0e8
Juliandev02 <julian@strawberryfoundations.xyz>
2024-07-14 16:54:54 +0200

    feat: added desktop view

---
+++ sbos_installer/views/desktop.py
@@ -0,0 +1,35 @@
+from sbos_installer.core.ui.screen import Screen
+from sbos_installer.core.process import Runner
+from sbos_installer.cli.parser import parse_bool
+from sbos_installer.cli.selection import ia_selection
+
+from rich.padding import Padding
+
+
+class DesktopView(Screen):
+    title = "Additional post-install steps"
+
+    def __init__(self, disk):
+        self.disk = disk
+
+        view = self.render
+        super().__init__(title=self.title, view=view)
+
+    def render(self):
+        self.console.print(Padding(
+            "A graphical desktop can be installed.\nThis is useful if you want to use this computer as a desktop.",
+            (0, 8))
+        )
+
+        confirm = parse_bool(ia_selection(
+            question="Would you like to install a graphical desktop?",
+            options=["Yes", "No"],
+            flags=["(Recommended for desktops)", "(Recommended for servers)"],
+            padding=8
+        ))
+
+        if confirm:
+            runner = Runner(True)
+            runner.run(f"chroot /mnt tasksel")
+
+        return None
