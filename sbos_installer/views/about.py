14524f6f180d0e240e7e2f3dca15cdb45ddd8915
Juliandev02 <julian@strawberryfoundations.xyz>
2024-07-14 15:05:09 +0200

    fix: fixed not showing cursor when exiting installation process

--- sbos_installer/views/about.py
+++ sbos_installer/views/about.py
@@ -22,7 +22,13 @@ class AboutView(Screen):
         ), justify="center")
 
         self.console.print("\nPress Enter to return", justify="center")
-        input()
+
+        try:
+            input()
+        except KeyboardInterrupt:
+            self.console.clear()
+            print(f"\n{YELLOW}Exited installation process{CRESET}")
+            self.console.show_cursor(True)
 
         print(CRESET)
         self.console.show_cursor(True)
