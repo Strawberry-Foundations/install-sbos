3b99ba646e929bef566efabdc89084bc1c52d70b
Juliandev02 <julian@strawberryfoundations.xyz>
2024-07-14 14:46:00 +0200

    feat: return back to selection screen

--- sbos_installer/views/about.py
+++ sbos_installer/views/about.py
@@ -14,13 +14,16 @@ class AboutView(Screen):
         super().__init__(title=self.title, view=view)
 
     def render(self):
+        self.console.show_cursor(False)
         self.console.print(Text.from_ansi(
-            f"StrawberryOS Installer v{version} (Nucleus)\n"
-            ""
+            f"StrawberryOS Installer v{version} (Nucleus)\n\n"
+            "The StrawberryOS Installer is an easy to use installer \n"
+            "that allows you to install StrawberryOS according to your preferences. "
         ), justify="center")
 
-        self.console.print(Text.from_ansi(f"Press Enter to return"), justify="center")
+        self.console.print(Text.from_ansi(f"\nPress Enter to return"), justify="center")
         input()
 
         print(CRESET)
+        self.console.show_cursor(True)
         return None
