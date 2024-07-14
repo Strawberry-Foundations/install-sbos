9d4def3023cc0a91699cfb522eb418201b0b6a4b
Juliandev02 <julian@strawberryfoundations.xyz>
2024-07-14 22:22:11 +0200

    feat: added Vars class

--- sbos_installer/var.py
+++ sbos_installer/var.py
@@ -1,1 +1,14 @@
 version = "2.0.2"
+
+
+class Vars:
+    def __init__(self):
+        self.os_type = None
+        self.hostname = None
+        self.net_stat = None
+        self.region = None
+        self.city = None
+        self.user_setup = None
+        self.disk_data = None
+        self.disk = None
+        self.packages = None
