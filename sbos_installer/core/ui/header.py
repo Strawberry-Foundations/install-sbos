66ebf7bc9f718e097cfc7b1231dd9eac8685c8d9
Juliandev02 <julian@strawberryfoundations.xyz>
2024-07-14 10:55:59 +0200

    fix: fixed header class

--- sbos_installer/core/ui/header.py
+++ sbos_installer/core/ui/header.py
@@ -1,16 +1,18 @@
 from sbos_installer.utils.screen import line_of_chars
+from sbos_installer.utils.colors import *
 
 from rich.console import Console
+from rich.text import Text
 
 
 class Header:
     border_style = "cyan on black"
-    text_style = "bold white on cyan"
+    text_style = "on cyan"
 
     def __init__(self, title):
         self.title = title
         console = Console()
 
         console.print(line_of_chars("▄"), style=self.border_style)
-        console.print(title, style=self.text_style, justify="center")
+        console.print(Text.from_ansi(f"{WHITE}{BOLD}{title}"), style=self.text_style, justify="center")
         console.print(line_of_chars("▀"), style=self.border_style)
