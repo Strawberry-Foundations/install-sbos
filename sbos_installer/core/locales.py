a56061db45dcf56d88ac368e826c68604b295436
Juliandev02 <julian@strawberryfoundations.xyz>
2024-07-20 19:48:29 +0200

    feat: move kbd layouts to separate file

---
+++ sbos_installer/core/locales.py
@@ -0,0 +1,7 @@
+kbd_layouts = {
+    "Deutsch (German)": "de-latin1",
+    "British English (English)": "uk",
+    "American English (English)": "us",
+    "Netherlands (Dutch)": "nl",
+    "-> Manual input": "own_layout",
+}
