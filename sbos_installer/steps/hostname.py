b471cc7c4bfb1e913a2b1f41893674a231ef1025
Juliandev02 <julian@strawberryfoundations.xyz>
2024-07-30 10:55:21 +0200

    fix: fix hostname configuration

--- sbos_installer/steps/hostname.py
+++ sbos_installer/steps/hostname.py
@@ -4,3 +4,6 @@ from sbos_installer.var import ROOT_MNT
 def configure_hostname(hostname: str):
     with open(f"{ROOT_MNT}etc/hostname", 'w') as file:
         file.write(hostname)
+
+    with open(f"{ROOT_MNT}etc/hosts", 'a') as file:
+        file.write(f"127.0.1.1 {hostname}")
