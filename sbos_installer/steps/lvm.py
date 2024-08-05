16a0772079bcebb74b9e30f5b091ff5cdab6824e
Juliandev02 <julian@strawberryfoundations.xyz>
2024-08-05 13:57:18 +0200

    feat: add file system switcher

--- sbos_installer/steps/lvm.py
+++ sbos_installer/steps/lvm.py
@@ -1,5 +1,6 @@
 from sbos_installer.core.process import Runner
 from sbos_installer.utils.colors import *
+from sbos_installer.views.error import ErrorView
 
 
 def get_partition_suffix(device):
@@ -12,7 +13,8 @@ def get_partition_suffix(device):
 def configure_lvm(disk, disk_data):
     runner = Runner(True)
     suffix = get_partition_suffix(disk)
-    disk_data = disk_data['disk'][disk]
+    file_system = disk_data["disk"]["file_system"]
+    disk_data = disk_data["disk"][disk]
 
     print(f"\n{GREEN}{BOLD}Formatting base partitions ...{CRESET}")
     runner.run(f"mkfs.fat -F 32 {disk}{suffix}1")
@@ -24,8 +26,16 @@ def configure_lvm(disk, disk_data):
     runner.run(f"lvcreate -L {disk_data['system']['size']}M -n system strawberryos")
     runner.run(f"lvcreate -l 100%FREE -n user strawberryos")
 
-    print(f"\n{GREEN}{BOLD}Formatting LVM volumes ...{CRESET}")
-    runner.run(f"mkfs.ext4 /dev/strawberryos/system")
-    runner.run(f"mkfs.ext4 /dev/strawberryos/user")
+    print(f"\n{GREEN}{BOLD}Formatting LVM volumes ({file_system}) ...{CRESET}")
+
+    match file_system:
+        case "btrfs":
+            runner.run(f"mkfs.btrfs /dev/strawberryos/system")
+            runner.run(f"mkfs.btrfs /dev/strawberryos/user")
+        case "ext4":
+            runner.run(f"mkfs.ext4 /dev/strawberryos/system")
+            runner.run(f"mkfs.ext4 /dev/strawberryos/user")
+        case _:
+            ErrorView(error_message="Invalid file system")
 
     print(f"\n{GREEN}{BOLD}Finished LVM configuration{CRESET}")
