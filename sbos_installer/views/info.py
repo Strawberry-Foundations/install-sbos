1f069839cba2f81a89042f0f173c6e5ca77a8dd1
Juliandev02 <julian@strawberryfoundations.xyz>
2024-08-29 21:13:46 +0200

    feat: add info view

---
+++ sbos_installer/views/info.py
@@ -0,0 +1,43 @@
+from sbos_installer import clear_screen
+from sbos_installer.core.ui.header import Header
+from sbos_installer.utils.colors import *
+
+from rich.console import Console
+from rich.text import Text
+
+import sys
+
+
+class InfoView:
+    class Header(Header):
+        border_style = "blue"
+        text_style = "on blue"
+
+    title = "Info"
+    console = Console()
+
+    def __init__(self, warning_message):
+        self.warning = warning_message
+
+        clear_screen()
+        InfoView.Header(self.title)
+
+        self.render()
+
+    def render(self):
+        self.console.show_cursor(False)
+        self.console.print(Text.from_ansi(
+            f"{self.warning}"
+        ), justify="center")
+
+        self.console.print("\nPress Enter to continue", justify="center")
+
+        try:
+            input()
+        except KeyboardInterrupt:
+            self.console.clear()
+            self.console.show_cursor(True)
+            sys.exit(1)
+
+        print(CRESET)
+        self.console.show_cursor(True)
