9a2301f525dfe523bfda21007ce382a499d77330
Juliandev02 <julian@strawberryfoundations.xyz>
2024-08-29 21:19:31 +0200

    fix: fix error handling

--- sbos_installer/views/about.py
+++ sbos_installer/views/about.py
@@ -1,6 +1,7 @@
 from sbos_installer.core.ui.screen import Screen
 from sbos_installer.var import version
 from sbos_installer.utils.colors import *
+from sbos_installer.views.info import InfoView
 
 from rich.text import Text
 
@@ -25,15 +26,7 @@ class AboutView(Screen):
 
         self.console.print("\nPress Enter to return", justify="center")
 
-        try:
-            input()
-        except KeyboardInterrupt:
-            self.console.clear()
-            self.console.print(Text.from_ansi(
-                f"-- {YELLOW}{BOLD}Exited installation process{CRESET} --"
-            ), justify="center")
-            self.console.show_cursor(True)
-            sys.exit(1)
+        input()
 
         print(CRESET)
         self.console.show_cursor(True)
