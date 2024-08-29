fdc26ef8da5496e42783c8c08087c8e924067372
Juliandev02 <julian@strawberryfoundations.xyz>
2024-08-29 20:56:51 +0200

    feat: move welcome/start view to separate file

---
+++ sbos_installer/views/start.py
@@ -0,0 +1,101 @@
+from sbos_installer.core.ui.screen import Screen
+from sbos_installer.core.ui.select_button import SelectButton, ia_selection
+from sbos_installer.core.process import run
+from sbos_installer.views.about import AboutView
+from sbos_installer.utils.colors import *
+
+from rich.text import Text
+
+import sys
+import os
+
+
+class StartView(Screen):
+    title = "Welcome to the StrawberryOS Installer!"
+
+    def __init__(self):
+        view = self.render
+        super().__init__(title=self.title, view=view)
+
+    def render(self):
+        self.console.show_cursor(False)
+        self.console.print(
+            f"Thanks for choosing StrawberryOS!\n"
+            f"The StrawberryOS Installer will guide you through the installation process.\n",
+            justify="center"
+        )
+
+        self.console.print(
+            Text.from_ansi(
+                f"{GREEN}{BOLD}! {CRESET}Use UP and DOWN to navigate, ENTER to continue\n\n"
+            ), justify="center"
+        )
+
+        group = []
+
+        SelectButton(
+            label=f"(->) Start installation",
+            description="Start with the installation of StrawberryOS",
+            group=group
+        )
+
+        SelectButton(
+            label=f"(>_) Open a console",
+            description="Open a console if you need to make changes beforehand. "
+                        "You can start the installer again using 'setup-strawberryos'",
+            group=group
+        )
+
+        SelectButton(
+            label=f"(?) About StrawberryOS Installer",
+            description="Learn more about the new StrawberryOS Installer (NucleusV2)",
+            group=group
+        )
+
+        SelectButton(
+            label=f"(!) Update StrawberryOS Installer",
+            description="Check whether there is a new update for the installer",
+            group=group
+        )
+
+        try:
+            selection = ia_selection(
+                question="",
+                options=group,
+                flags=["start", "console", "about", "update"]
+            )
+
+        except KeyboardInterrupt:
+            self.console.clear()
+            self.console.print(Text.from_ansi(
+                f"-- {YELLOW}{BOLD}Exited installation process{CRESET} --"
+            ), justify="center")
+            self.console.show_cursor(True)
+            sys.exit(0)
+
+        match selection:
+            case "console":
+                self.console.clear()
+
+                try:
+                    with open("/etc/motd", 'r') as _motd:
+                        print(_motd.read(), end="")
+
+                except (FileNotFoundError, Exception):
+                    pass
+
+                sys.exit(0)
+
+            case "about":
+                AboutView()
+                self.console.clear()
+                self.redraw()
+
+            case "update":
+                run("update-installer")
+                self.console.clear()
+                python = sys.executable
+                os.execv(python, ['python3'] + sys.argv)
+
+        self.console.show_cursor(True)
+        return None
