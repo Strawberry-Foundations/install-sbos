d13571a968b2efdd1e114e5b3a2713ba685916ad
Juliandev02 <julian@strawberryfoundations.org>
2025-01-18 19:23:02 +0100

    fix: update version import to use uppercase constant

--- sbos_installer/views/about.py
+++ sbos_installer/views/about.py
@@ -1,5 +1,5 @@
 from sbos_installer.core.ui.screen import Screen
-from sbos_installer.var import version
+from sbos_installer.var import VERSION
 from sbos_installer.utils.colors import *
 from sbos_installer.views.info import InfoView
 
@@ -18,7 +18,7 @@ class AboutView(Screen):
     def render(self):
         self.console.show_cursor(False)
         self.console.print(Text.from_ansi(
-            f"StrawberryOS Installer v{version} (Nucleus)\n\n"
+            f"StrawberryOS Installer v{VERSION} (Nucleus)\n\n"
             "The StrawberryOS Installer is an easy to use installer \n"
             "that allows you to install StrawberryOS according to your preferences.\n\n"
             f"{LIGHT_BLUE}https://github.com/Strawberry-Foundations/install-sbos{CRESET}"
